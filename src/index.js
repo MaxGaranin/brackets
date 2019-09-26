3840000b8c4b983a7be2feed5b159fb57e8cae6c
GaraninMS <maxgaranin@gmail.com>
2019-09-26 16:02:39 +0400

    feat: realize algorithm for task 'Brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let beginBrackets = [];
+  let endBrackets = [];
+  let endToBeginMap = {};
+  let equalBrackets = [];
+
+  bracketsConfig.forEach(cfg => {
+    let beginBr = cfg[0];
+    let endBr = cfg[1];
+
+    beginBrackets.push(beginBr);
+    endBrackets.push(endBr);
+    endToBeginMap[endBr] = beginBr;
+
+    if (beginBr === endBr) {
+      equalBrackets.push(beginBr);
+    }
+  });
+
+  let stack = [];
+ 
+  for (const ch of str) {
+    let isFoundEqualBr = false;
+    if (equalBrackets.indexOf(ch) >= 0) {
+      isFoundEqualBr = true;
+    }
+
+    if (beginBrackets.indexOf(ch) >= 0) {
+      if (isFoundEqualBr && stack.length > 0) {
+        var br = stack[stack.length - 1];
+        if (br == ch) {
+          stack.pop();
+        }
+        else {
+          stack.push(ch);
+        }
+      }
+      else {
+        stack.push(ch);
+      }
+    }
+    else if (endBrackets.indexOf(ch) >= 0) {
+      if (stack.length == 0) return false;
+
+      let br = stack.pop();
+      if (endToBeginMap[ch] != br) {
+        return false;
+      }
+    }
+    else {
+      continue;
+    }
+  }
+
+  return stack.length == 0;
 }
